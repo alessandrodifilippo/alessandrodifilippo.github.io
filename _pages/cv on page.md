ccc0a36dcca333143a8e6af15fe4496d72e5076c
alessandrodifilippo <197920650+alessandrodifilippo@users.noreply.github.com>
2026-02-04 15:35:44 +0000

    Deploying to gh-pages from @ alessandrodifilippo/alessandrodifilippo.github.io@7c3df647a376cc6b1b5120432f619ab2e2508265 🚀

---
+++ _pages/cv on page.md
@@ -0,0 +1,12 @@
+<!-- ---
+layout: page
+permalink: /cv2/
+title: cv
+description:
+nav: true
+nav_order: 2
+--- -->
+
+<object data="/assets/pdf/Curriculum%20ADF.pdf" width="1300" height="1300" type='application/pdf'/>
+<object data="/assets/pdf/Curriculum%20ADF.pdf" width="1000" height="1000" type='application/pdf'/>
+<object data="/assets/pdf/Curriculum%20ADF.pdf" width="900" height="900" type='application/pdf'/>
--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
-          title: "cv",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv2/";
-          },
-        },{id: "nav-research",
+  },{id: "nav-research",
           title: "research",
           description: "",
           section: "Navigation",
